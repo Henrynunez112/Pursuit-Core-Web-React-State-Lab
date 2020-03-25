1a7dd9f3c5f5ba9288d30a782188ad74d8d7c208
Henrynunez112 <henrynunez@pursuit.org>
2020-03-25 09:48:18 -0400

    working on clicker

--- src/Component/FirstClicker.js
+++ src/Component/FirstClicker.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
 class FirstClicker extends React.Component{
-    state = {count: 0}
+    state = {count: 0};
+    increase = {add: 1};
+
 
     handleClick = () =>{
-        this.setState({
-            count: this.state.count + 1
-        })
+        if(this.state.count < 100){
+            this.setState(()=>{
+                return {count: this.state.count + this.increase.add}
+            })
+        }else{
+            alert("You Won");
+            this.setState(() =>{
+                return (
+                    this.state.count = 0,
+                    this.increase.add = 1
+                )
+            })
+        }
+    }
+
+    pointsAdded = () =>{
+        if(this.state.count - 10 < 0){
+            alert('You do not have enough points');
+        }else{
+            this.setState(() => {
+                return(
+                    this.increase.add + 1,
+                    this.state.count - 10
+                )
+            })
+        }
     }
 
     render(){
@@ -14,7 +39,8 @@ class FirstClicker extends React.Component{
         return(
             <div>
                 <h1>Current Score: {count}</h1>
-        <button onClick={this.handleClick}>+1</button>
+                <button onClick={this.handleClick}>+{this.increase.add}</button>
+                <button onClick={this.pointsAdded}>Pay 10 points to change from +{this.increase.add} to +{this.increase.add + 1}</button>
             </div>
         )
     }
