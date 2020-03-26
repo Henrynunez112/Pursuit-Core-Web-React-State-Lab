8ae3550e067d7e4c4d8aded6485bcd486909bf5a
Henrynunez112 <henrynunez@pursuit.org>
2020-03-25 20:08:25 -0400

    just needs css

--- src/Component/FirstClicker.js
+++ src/Component/FirstClicker.js
@@ -1,46 +1,50 @@
 import React from 'react';
 
 class FirstClicker extends React.Component{
-    state = {count: 0};
-    increase = {add: 1};
+    state = {count: 0, add: 1, won: false};
 
 
-    handleClick = () =>{
+    handleClick = (e) =>{
+        // debugger
         if(this.state.count < 100){
-            this.setState(()=>{
-                return {count: this.state.count + this.increase.add}
-            })
-        }else{
+            this.setState((prevState, props)=>{
+                return {count: this.state.count + this.state.add}
+            });
+        }else if(this.state.count >= 100){
             alert("You Won");
-            this.setState(() =>{
-                return (
-                    this.state.count = 0,
-                    this.increase.add = 1
-                )
-            })
+            this.setState({count: 0,
+                            add: 1,
+                            won: true})
         }
     }
 
-    pointsAdded = () =>{
+    pointsAdded = (e) =>{
         if(this.state.count - 10 < 0){
             alert('You do not have enough points');
         }else{
-            this.setState(() => {
-                return(
-                    this.increase.add + 1,
-                    this.state.count - 10
-                )
+            this.setState({
+                count: this.state.count - 10,
+                add: this.state.add + 1
             })
         }
+    }
+    handleReset = () =>{
+        this.setState({
+            count: 0,
+            add: 1,
+            won: false
+        })
     }
 
     render(){
         const { count } = this.state;
+        const button = this.state.won ? <button onClick={this.handleReset}>Reset</button> : null
         return(
             <div>
                 <h1>Current Score: {count}</h1>
-                <button onClick={this.handleClick}>+{this.increase.add}</button>
-                <button onClick={this.pointsAdded}>Pay 10 points to change from +{this.increase.add} to +{this.increase.add + 1}</button>
+                <button onClick={this.handleClick}>+{this.state.add}</button>
+                <button onClick={this.pointsAdded}>Pay 10 points to change from +{this.state.add} to +{this.state.add + 1}</button>
+                {button}
             </div>
         )
     }
